35bb1e7b92cef91b95a698c37f0fa9283ffdefa3
chris_kulhanek <156239549+chriskulhan@users.noreply.github.com>
2025-02-04 00:51:29 +0000

    copied_photo_in_chunk

--- 9_copy_a_picture_file.py
+++ 9_copy_a_picture_file.py
@@ -11,8 +11,27 @@
 #will need to read and write in binary in order to copy the file:
 #change r to rb and w to wb
 
+# with open ('IMG_7178.JPG', 'rb') as rf:
+#     with open ('IMG_7178_copy.JPG', 'wb') as wf:
+#         for line in rf:
+#             wf.write(line)
+            #made a copy of the photo!!!! (I'm shook!)
+
+#Maybe you want to do this is different chunk sizes:
 with open ('IMG_7178.JPG', 'rb') as rf:
     with open ('IMG_7178_copy.JPG', 'wb') as wf:
-        for line in rf:
-            wf.write(line)
-            #made a copy of the photo!!!! (I'm shook!)
+        #create a chunk size:
+        chunk_size = 4096
+        #read each chunk size in turn
+        rf_chunk = rf.read(chunk_size)
+        #create a loop to read until there is nothing left to read in our copy:
+        #while the length of the chunk is greater than zero
+        while len(rf_chunk) > 0:
+            #keep reading
+            wf.write(rf_chunk)
+            #make sure it's not an infinite loop read in the next chunk size from the original file:
+            rf_chunk = rf.read(chunk_size)
+
+            #delete copy to see if it works again
+
+            #It worked again!!! (but it took a little longer.)
